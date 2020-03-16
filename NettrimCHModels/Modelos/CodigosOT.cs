9b29887e198ed9b49d4ff85b7f26b566386b44b2
Francisco Lorente <francislorenruiz@outlook.es>
2020-03-16 10:11:54 +0100

    Añadido modelo CodigosOT

---
+++ NettrimCHModels/Modelos/CodigosOT.cs
@@ -0,0 +1,24 @@
+﻿namespace NettrimCHModels.Modelos
+{
+    public class CodigosOT
+    {
+        public int CodigoID { get; set; }
+        public string Nombre { get; set; }
+        public string Alias { get; set; }
+        public string FechaInicio { get; set; }
+        public string FechaFin { get; set; }
+        public string Descripcion { get; set; }
+
+        public int CodigoCliente { get; set; }
+        public string JP { get; set; }
+        public bool Interna { get; set; }
+        public string Cliente { get; set; }
+        public string Proyecto { get; set; }
+        public bool Facturable { get; set; }
+        public bool Informe { get; set; }
+        public string Area { get; set; }
+        public string Plataforma { get; set; }
+        public bool ProyectoCerrado { get; set; }
+        public int TipoProyecto { get; set; }
+    }
+}
