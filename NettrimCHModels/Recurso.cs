2f8fba26f61c85e45176167da1a2c8b90f871e0c
Alejandro Gaspar <AlejandroWork97@hotmail.com>
2020-03-16 10:41:40 +0100

    DataAnnotations Removed

--- NettrimCHModels/Recurso.cs
+++ NettrimCHModels/Recurso.cs
@@ -7,29 +7,13 @@ namespace NettrimCHModels.Recursos
 {
     public class Recurso
     {
-        [Key]
-        [Required(ErrorMessage = "El Id del Recurso es necesario")]
-        [StringLength(4)]
         public string IdRecurso { get; set; }
-
-        [StringLength(50)]
         public string Nombre { get; set; }
-
-        [Range(4, int.MaxValue, ErrorMessage = "Introduce una clave correcta")]
         public int Clave { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-   
-        public bool Baja { get; set; }
-
-        [StringLength(50)]
+        public string Email { get; set; }  
+        public bool Baja { get; set; }       
         public string Apellido1 { get; set; }
-
-        [StringLength(50)]
         public string Apellido2 { get; set; }
-
-        [RegularExpression("^[0-9]{8,8}[A-Za-z]$"),]
         public string DNI { get; set; }
     }
 }
