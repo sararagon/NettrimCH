82c94549dde894999ae11ff3090a6ba9c03702c1
Sara Porcar Palacios <sararagon.5@live.com>
2020-03-16 09:31:14 +0100

    Modelo Cliente

---
+++ NettrimCHModels/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NettrimCHModels
+{
+    class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Codigo { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Responsable { get; set; }
+
+        public string Telefono { get; set; }
+    }
+}
