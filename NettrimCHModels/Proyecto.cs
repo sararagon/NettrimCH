b3b64b7f2b9d1e56ac8b5048507758e43c9dfa87
Francisco Lorente <francislorenruiz@outlook.es>
2020-03-16 11:06:19 +0100

    Modificación modelo y añadido modelo Proyecto

---
+++ NettrimCHModels/Proyecto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NettrimCHModels
+{
+    public class Proyecto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public bool Cerrado { get; set; }
+        public int Tipo { get; set; }
+    }
+}
