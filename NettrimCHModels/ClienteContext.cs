2947bb825f07ef1bc34dbe8b1563b44803bb73b6
Sara Porcar Palacios <sararagon.5@live.com>
2020-03-16 09:55:15 +0100

    ClienteContext

---
+++ NettrimCHModels/ClienteContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NettrimCHModels
+{
+    class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+        }
+         
+ 
+        public DbSet<Cliente> Clientes { get; set; }
+        
+    }
+}
