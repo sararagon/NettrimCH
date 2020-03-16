95d4d91e1b335473460b5dd814195ee13576dd33
LuisVallespin <lvallespinabad@gmail.com>
2020-03-16 10:46:39 +0100

    Modelo TipoTarea hecho

---
+++ NettrimCHModels/Modelos/TipoTarea.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NettrimCHModels.Modelos
+{
+    class TipoTarea
+    {
+        public string Nombre { get; set; }
+    }
+}
