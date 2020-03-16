b48878282ddf8667e867aa92d81a5a0bc3511edd
LuisVallespin <lvallespinabad@gmail.com>
2020-03-16 13:25:28 +0100

    Generic repository

---
+++ NettrimCHServices/Repository/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NettrimCHServices.Repository.GenericRepository
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(object id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(object id);
+        Task Save();
+    }
+}
