7b00a9966ddcab1782360b8a5287bea97f18b686
Sara Porcar Palacios <sararagon.5@live.com>
2020-03-16 13:12:19 +0100

    Context

---
+++ NettrimCHServices/DAL/NettrimCHContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NettrimCHModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NettrimCHServices.DAL
+{
+    class NettrimCHContext : DbContext
+    {
+        public NettrimCHContext(DbContextOptions<NettrimCHContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Proyecto> Proyectos { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+}
